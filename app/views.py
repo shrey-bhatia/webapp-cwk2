10ca0a822e612e11d315c4e3fbf6db8e2ee16636
Shrey Bhatia <shrey.apply@gmail.com>
2023-10-31 06:30:01 +0000

    added Navbar and made it WCAG friendly

--- app/views.py
+++ app/views.py
@@ -1,5 +1,40 @@
+from flask import render_template
 from app import app
 
+
 @app.route('/')
-def index():
-    return "Hello World!!!"
+def home():
+    user = {'name': 'Sam Wilson'}
+    return render_template('base.html',
+                           title='Home page',
+                           user=user)
+
+
+@app.route('/addGoalPage')
+def goal():
+    return render_template('base.html',
+                           title='addGoalPage')
+
+
+@app.route('/allExpensesPage')
+def allexpensespage():
+    return render_template('base.html',
+                           title='allExpensesPage')
+
+
+@app.route('/addIncomePage')
+def addincomepage():
+    return render_template('base.html',
+                           title='addIncomePage')
+
+
+@app.route('/allIncomesPage')
+def allincomespage():
+    return render_template('/base.html',
+                           title='allIncomesPage')
+
+
+@app.route('/addExpensePage')
+def addexpensepage():
+    return render_template('base.html',
+                           title='addExpensePage')
