7192cd2d74018728027ce43c1bcb5dd5bd7ebd54
Shrey Bhatia <shrey.apply@gmail.com>
2023-11-01 05:20:31 +0000

    added forms to addIncome, addGoal and addExpense pages with client-side validation

--- app/views.py
+++ app/views.py
@@ -5,36 +5,36 @@ from app import app
 @app.route('/')
 def home():
     user = {'name': 'Sam Wilson'}
-    return render_template('base.html',
-                           title='Home page',
+    return render_template('homePage.html',
+                           title='Home Page',
                            user=user)
 
 
 @app.route('/addGoalPage')
 def goal():
-    return render_template('base.html',
-                           title='addGoalPage')
+    return render_template('addGoalPage.html',
+                           title='Goal')
 
 
 @app.route('/allExpensesPage')
 def allexpensespage():
-    return render_template('base.html',
-                           title='allExpensesPage')
+    return render_template('allExpensesPage.html',
+                           title='All Expenses')
 
 
 @app.route('/addIncomePage')
 def addincomepage():
-    return render_template('base.html',
-                           title='addIncomePage')
+    return render_template('addIncomePage.html',
+                           title='Add Income')
 
 
 @app.route('/allIncomesPage')
 def allincomespage():
-    return render_template('/base.html',
-                           title='allIncomesPage')
+    return render_template('/allIncomesPage.html',
+                           title='All Incomes')
 
 
 @app.route('/addExpensePage')
 def addexpensepage():
-    return render_template('base.html',
-                           title='addExpensePage')
+    return render_template('addExpensePage.html',
+                           title='Add Expenses')
